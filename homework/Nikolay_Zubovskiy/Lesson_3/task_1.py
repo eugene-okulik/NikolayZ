afa4523653a81700e2bc69f6481be7d0be767327
Nikolay Zubovskiy <purrweb@MacBook-Air-purrweb.local>
2026-01-25 23:13:52 +0300

    Lesson_3 done

---
+++ homework/Nikolay_Zubovskiy/Lesson_3/task_1.py
@@ -0,0 +1,9 @@
+num_1 = 56
+num_2 = 91
+sum_result = num_1 + num_2
+diff_result = num_1 - num_2
+mult_result = num_1 * num_2
+print('Даны два числа:', num_1, 'и', num_2)
+print('Сумма чисел:', sum_result)
+print('Разность чисел(в заданном порядке):', diff_result)
+print('Произведение чисел:', mult_result)
