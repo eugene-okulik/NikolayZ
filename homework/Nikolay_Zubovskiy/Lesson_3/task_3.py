bf96d887b686d7186e1a230d7871882c96aa58b5
Nikolay Zubovskiy <purrweb@MacBook-Air-purrweb.local>
2026-01-26 06:46:35 +0300

    Lesson_3 fixed_2 lint format

--- homework/Nikolay_Zubovskiy/Lesson_3/task_3.py
+++ homework/Nikolay_Zubovskiy/Lesson_3/task_3.py
@@ -1,6 +1,6 @@
 num_1 = 201
 num_2 = 4099
-average_arithmetic = (num_1 + num_2)/2
+average_arithmetic = (num_1 + num_2) / 2
 average_geometric = (num_1 * num_2)**0.5
 print('Даны два числа:', num_1, 'и', num_2)
 print('Среднее арифметическое чисел:', average_arithmetic)
