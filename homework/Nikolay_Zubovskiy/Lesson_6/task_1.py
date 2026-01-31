84c439a3b6e8df76ee17a4895e8141ce04c762aa
Nikolay Zubovskiy <purrweb@MacBook-Air-purrweb.local>
2026-01-31 14:33:49 +0300

    changed task_1 refact with AI for me

--- homework/Nikolay_Zubovskiy/Lesson_6/task_1.py
+++ homework/Nikolay_Zubovskiy/Lesson_6/task_1.py
@@ -3,7 +3,7 @@
 UNDER = '\033[4m'
 RESET = '\033[0m'
 
-_ing = (UNDER + 'ING' + RESET)
+ING = UNDER + 'ING' + RESET
 text = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. '
         'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero')
 text_list = text.split()
@@ -13,10 +13,12 @@ for word in text_list:
     # if word[-1] == ',' or word[-1] == '.':
     # if word.endswith(',') or word.endswith('.'):
     if word[-1] in '(,.)':
-        # word = word[:-1] + _ing + word[-1]
-        word = '{0}{1}{2}'.format(word[:-1], _ing, word[-1])
+        # word = '{0}{1}{2}'.format(word[:-1], _ing, word[-1])
+        # word = word[:-1] + ING + word[-1]
+        word = f"{word[:-1]}{ING}{word[-1]}"
     else:
-        word = word + _ing
+        # word = word + ING
+        word = f'{word}{ING}'
     text_list_new.append(word)
 
 text_new = ' '.join(text_list_new)
