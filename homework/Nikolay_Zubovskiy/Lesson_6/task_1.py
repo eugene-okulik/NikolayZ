04974f4adb3838094d35d07a46e0c25e99a38861
Nikolay Zubovskiy <purrweb@MacBook-Air-purrweb.local>
2026-01-31 13:58:25 +0300

    Lesson_6 done

---
+++ homework/Nikolay_Zubovskiy/Lesson_6/task_1.py
@@ -0,0 +1,24 @@
+
+# формат-коды для подчеркиваия текста, текст между будет подчеркнут(источник чат gpt)
+UNDER = '\033[4m'
+RESET = '\033[0m'
+
+_ing = (UNDER +'ING'+ RESET)
+text = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. '
+        'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+        )
+text_list = text.split()
+text_list_new = []
+
+for word in text_list:
+    # if word[-1] == ',' or word[-1] == '.':
+    # if word.endswith(',') or word.endswith('.'):
+    if word[-1] in '(,.)':
+        # word = word[:-1] + _ing + word[-1]
+        word = '{0}{1}{2}'.format(word[:-1], _ing, word[-1])
+    else:
+       word = word + _ing
+    text_list_new.append(word)
+
+text_new = ' '.join(text_list_new)
+print(f'"{text_new}"')
